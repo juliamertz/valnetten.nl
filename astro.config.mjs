66d3a331443f497494a66f9a8ecc60479c085aaa
jorismertz <joris.mertz@gmail.com>
2022-11-11 10:14:30 +0100

    add image optimization & seo tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap(), robotsTxt(), prefetch()],
+  vite: {
+    ssr: {
+      external: ["@11ty/eleventy-img"],
+    },
+  },
 });
